7966b385f090e1988e0dd928c222a14e1b611a7e
Francesco Valente <francesco83.valente@gmail.com>
2013-10-29 15:20:00 +0100

    Added grunt tasks for development and production

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,10 @@
+'use strict';
+
 module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    },
 
     clean: ["www/"],
 
@@ -18,14 +14,55 @@ module.exports = function(grunt) {
           {expand: true, cwd: 'src/', src: ['**'], dest: 'www/'}
         ]
       }
+    },
+
+    watch: {
+      files: ['src/**/*'],
+      tasks: ['default'],
+    },
+
+    connect: {
+      dev: {
+        options: {
+          hostname: "*",
+          port: 8000,
+          base: 'src'
+        }
+      },
+      prod: {
+        options: {
+          hostname: "*",
+          port: 8000,
+          base: 'www'
+        }
+      }
+    },
+
+    requirejs: {
+      compile: {
+        options: {
+          appDir: 'src',
+          mainConfigFile: 'src/js/main.js',
+          dir: 'www',
+          modules: [
+            {
+                name: '../main',
+                include: ['jquery', 'jquerymobile', 'jquery.loadTemplate-1.2.4']
+            },
+            {
+                name: '../index',
+                exclude: ['../main', '../../cordova']
+            }
+          ]
+        }
+      }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-copy');
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-  grunt.registerTask('build', ['clean', 'copy']);
+  grunt.registerTask('serve:dev', ['connect:dev', 'watch']);
+  grunt.registerTask('serve:prod', ['clean', 'requirejs', 'connect:prod:keepalive']);
+  grunt.registerTask('build', ['clean', 'requirejs']);
   grunt.registerTask('default', ['build']);
-
 };
--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,13 @@
 require.config({
     baseUrl: 'js/vendor',
     paths: {
-        jquery: 'jquery-2.0.3'
+        jquery: 'jquery-2.0.3',
+        jquerymobile: 'jquery.mobile-1.3.2'
     },
 
     shim: {
-        "jquery.mobile-1.3.2": ["jquery"],
         "jquery.loadTemplate-1.2.4": ["jquery"]
     }
 });
 
-requirejs(["../index"]);
+// requirejs(["../index"]);
